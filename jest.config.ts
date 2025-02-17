e61ad54896d47cd210464c279a1c052685ad423f
James Stephenson <jstep.ux@gmail.com>
2025-02-17 16:29:46 -0600

    Jest testing setup

---
+++ jest.config.ts
@@ -0,0 +1,20 @@
+import type { Config } from '@jest/types';
+
+const config: Config.InitialOptions = {
+    preset: 'ts-jest',
+    testEnvironment: 'node',
+    roots: ['<rootDir>'],
+    modulePaths: ['.'],
+    moduleDirectories: ['node_modules', 'src'],
+    moduleNameMapper: {
+        '^@/(.*)$': '<rootDir>/src/$1',
+    },
+    testMatch: [
+        '**/__tests__/**/*.test.ts',
+    ],
+    transform: {
+        '^.+\\.tsx?$': 'ts-jest',
+    },
+};
+
+export default config;
