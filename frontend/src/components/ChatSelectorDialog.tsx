aefba4b3a01f28c5ff66db62a124f6fc976c0558
James Stephenson <jstep.ux@gmail.com>
2025-04-07 15:28:55 -0500

    Missed a spot

--- frontend/src/components/ChatSelectorDialog.tsx
+++ frontend/src/components/ChatSelectorDialog.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { X } from 'lucide-react';
 import ChatSelector from './ChatSelector';
 import { CharacterCard } from '../types/schema';
